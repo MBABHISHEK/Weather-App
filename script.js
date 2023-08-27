4e07d6accff484e7749100e25ae6eee5d2048fc9
ABHISHEK M B <117739148+MBABHISHEK@users.noreply.github.com>
2023-08-27 23:12:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-/*
-let cityInput =document.querySelector('.container .input-box input');
-let serachBtn=document.querySelector('.container .input-box button');
-let weather_img=document.querySelector('.container .weather-img img');
-let temp=document.querySelector('.container .temp');
-let weatherDescrip=document.querySelector('.container .weather_descrip');
-let humidity=document.querySelector('.container .other .humidity-value');
-let windspeed=document.querySelector('.container .wind-speed .windspeed-value');
-
-serachBtn.addEventListener('click',() =>{
-    if(cityInput.value != '')
-    {
-        getWeatherInfo();
-    }
-});
-
-
-let getWeatherInfo = () => {
-    let city = cityInput.value;
-    let apiKey = 'a8c1d2697dd49736798e025886e20064'; // Replace this with your actual API key
-    let url = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric';
-
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            weather_img.src = 'https://api.openweathermap.org/img/w/${data.weather[0].icon}.png';
-            temp.innerHTML = '${data.main.temp}&#176';
-            weatherDescrip.innerHTML =data.weather[0].description;
-            windspeed.innerHTML = '${data.wind.speed} Km/H';
-            humidity.innerHTML = '${data.main.humidity}%';
-        });
-}
-
-    
-getWeatherInfo();
-*/
 let cityInput = document.querySelector('.container .input-box input');
 let searchBtn = document.querySelector('.container .input-box button');
 let weather_img = document.querySelector('.container .weather-img img');
@@ -50,7 +14,7 @@ searchBtn.addEventListener('click', () => {
 
 let getWeatherInfo = () => {
     let city = cityInput.value;
-    let apiKey = 'a8c1d2697dd49736798e025886e20064'; // Replace this with your actual API key
+    let apiKey = ''; // Replace this with your actual API key
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(url)
